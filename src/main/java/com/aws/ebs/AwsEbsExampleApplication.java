9e91a032a63d0f4862532929d9a8b207612897f8
Ashok921 <90255130+Ashok921@users.noreply.github.com>
2022-03-24 13:45:43 +0530

    Initial commit

---
+++ src/main/java/com/aws/ebs/AwsEbsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEbsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEbsExampleApplication.class, args);
+	}
+
+}
